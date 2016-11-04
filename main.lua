45c0bb9a588f5c41ffb37dc89bec53e89f3c91b0
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2016-11-04 19:55:35 -0200

    second

--- main.lua
+++ main.lua
@@ -1,10 +1,11 @@
+local bump = require 'bump'
 
 function startworld(seed)
-    local world = {}
-    world.gravity = 1
+    local world = bump.newWorld()
+    world.gravity = 0.5
     local player = {}
     player.x = 10
-    player.y = 30
+    player.y = 50
     player.vy = 0
     player.w = 10
     player.h = 10
@@ -12,12 +13,16 @@ function startworld(seed)
     player.jump = 10
     player.state = "air"
     world.player = player 
+    world:add(world.player,world.player.x,world.player.y,world.player.w,world.player.h)
     world.plats = {}
     table.insert(world.plats,{x=0,y=0,h=5,w=40})
     local num = 10
     local sw = 15
     for i=1,num do
         table.insert(world.plats,{x=20+i*(40+sw),y=0,h=5,w=30})
+    end
+    for k,v in pairs(world.plats) do
+        world:add(v,v.x,v.y,v.w,v.h)
     end
     return world
 end
@@ -26,7 +31,11 @@ function collide(o, world)
         if o.x < p.x+p.w and o.x > p.x and o.y > p.y and o.y < p.y+o.h then
             return true
         end
+        if p.x < o.x+o.w and p.x > o.x and p.y > o.y and p.y < o.y+p.h then
+            return true
+        end
     end
+    return false
 end
 function update(world,input,dt)
     newworld = {}
@@ -44,7 +53,7 @@ function update(world,input,dt)
     elseif input == "jump" then
         if world.player.state == "ground" then
             newworld.player.vy = world.player.jump
-            newworld.player.state = "air"
+            --newworld.player.state = "air"
         end
     end
     if world.player.state == "air" then
@@ -76,7 +85,9 @@ end
 function max(t)
     maxv = nil
     maxk = nil
+    --print(t)
     for k,v in pairs(t) do
+        --print(k,v)
         if maxv == nil or v > maxv then  
             maxv = v
             maxk = k
@@ -84,33 +95,42 @@ function max(t)
     end
     return maxk
 end
-function initIA(size)
+function initIA(size0,size1,size2,size3)
     local ia = {}
-    for i=1,size do
-        --ia[i] = {100,100,100,100}
-        ia[i] = {0,0,0,100}
+    for i=1,size0 do
+        ia[i] = {}
+        for k,v in pairs({"air","ground"}) do
+            ia[i][v] = {}
+            for j=-size2/2,size2/2 do
+                ia[i][v][j] = {}
+                for k=-size3/2,size3/2 do
+                --ia[i] = {100,100,100,100}
+                    ia[i][v][j][k] = {100,100,100,100}
+                end
+            end
+        end
     end
     return ia
 end
 --rate = 1
-function learning(rate, security,num,length)
-    local IA = initIA(length+10)
+function learning(rate, security,num,length,range)
+    local IA = initIA(length+10,2,range+10,range+10)
     seed = 0
     for i=1,num do
         print("episode "..i)
         IA = episode(IA,rate,security,seed,length)
     end
     for k,v in pairs(IA) do
-        print(k, v[1],v[2],v[3],v[4])
+        --print(k, v["air"],v[2],v[3],v[4])
     end
     return IA
 end
     
 function runIA(ia,world,s)
-    return max(ia[s])
+    return max(getstate(ia,s))
 end
-function train(ia,s,a,score,rate,security,dt)
-    ia[s][a] = ia[s][a] + rate * (score + security*max(ia[s+dt]) - ia[s][a] )
+function train(ia,s,sl,a,score,rate,security,dt)
+    getstate(ia,s)[a] = getstate(ia,s)[a] + rate * (score + security*max(getstate(ia,sl)) - getstate(ia,s)[a] )
     return ia
 end
 function condition(world)
@@ -123,25 +143,57 @@ function remember(x)
     return x
 end
 function getscore(world,newworld)
-    --if newworld.player.state == "ground" and world.player.state == "air" then
-    --    return 10 + (newworld.player.x - world.player.x)/100
-    --end
+    local score = 0
+    --[[
+    if newworld.player.state == "ground" and world.player.state == "air" then
+        score = score + 10
+    end
     if newworld.player.state == "ground" and world.player.state == "ground" then
-        return (newworld.player.x - world.player.x)/100
+        score = score + 1
     end
     if newworld.player.state == "air" then
-        return (newworld.player.y - world.player.y) + (newworld.player.x - world.player.x)/100
+        --score = score + (newworld.player.y - world.player.y)
     end
+    --]]
+    score = score - (newworld.player.x - world.player.x)
     return 0
 end
-function getstate(time,world)
-    return time 
+function dist(a,b)
+    return math.sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y))
 end
+function genstate(time,world)
+    local plata,dx,dy
+    local d = nil
+    --[[
+    for k,p in pairs(world.plats) do
+        local di = dist(world.player,p)
+        if d == nil or di < d then
+            d = di
+            plata = p
+        end
+    end
+    --]]
+    plata = world.plats[2]
+    dx = math.floor(plata.x - world.player.x) 
+    dy = math.floor(plata.y - world.player.y) 
+    --print(dx,dy)
+    return {time,world.player.state,dx,dy} 
+end
+function getstate(ia,s)
+    --player state,dx,dy
+    --print(s[1],s[2],s[3])
+    if s[3] > r/2 then s[3] = r/2 end
+    if s[3] < -r/2 then s[3] = -r/2 end
+    if s[4] > r/2 then s[4] = r/2 end
+    if s[4] < -r/2 then s[4] = -r/2 end
+    return ia[s[1]][s[2]][s[3]][s[4]] 
+end
+
 function input2string(i)
-    if i == 1 then return "left" end
+    if i == 1 then return "idle" end
     if i == 2 then return "right" end
     if i == 3 then return "jump" end
-    if i == 4 then return "idle" end
+    if i == 4 then return "left" end
 end
 function episode(memory,rate,security,seed,length)
     local IA = remember(memory)
@@ -150,31 +202,39 @@ function episode(memory,rate,security,seed,length)
     local score = 0
     --print("\tstart")
     while time < length and not condition(world) do
-        local input = runIA(IA,world,time)
+        local s = genstate(time,world)
+        local input = runIA(IA,world,s)
         local dt = 1
         local newworld = update(world,input2string(input),dt)
         --print(input2string(input),world.player.x,newworld.player.x)
         score = score + getscore(world,newworld) - 1
         --print("\tstep",time,input,score, world.player.x,world.player.y)
-        IA = train(IA,getstate(time,world),input,score,rate,security,dt)
         time = time + dt
+        local sl = genstate(time,world)
+        IA = train(IA,s,sl,input,score,rate,security,dt)
         world = newworld
     end
+    print(score)
     --print(time,IA[time][1],IA[time][2],IA[time][3],IA[time][4])
     return IA
 end
 function love.load()
-    l = 50
-    trained = learning(1,0.5,100,l)
+    l = 100
+    r = 50
+    trained = learning(1,0.5,100,l,r)
     world = startworld(seed)
     time = 1
     H = 400
+    score = 0
 end
 function love.update(dt)
-    if time < l then
-        local input = runIA(trained,world,time)
+
+    if time < l and love.keyboard.isDown("space") then
+        local s = genstate(time,world)
+        local input = runIA(trained,world,s)
     --else
     --    local input = 4
+        score = score + getscore(world,newworld) - 1
         local newworld = update(world,input2string(input),1)
         time = time + 1
         world = newworld
@@ -189,6 +249,9 @@ function love.draw()
     end
     love.graphics.rectangle("line",world.player.x,H-world.player.y-world.player.h,world.player.w,world.player.h)
     love.graphics.setColor(255,255,255)
+    love.graphics.print(score,10,10)
+    love.graphics.print(world.player.x,10,20)
+    love.graphics.print(world.player.y,10,30)
     for k,p in pairs(world.plats) do
         love.graphics.rectangle("line",p.x,H-p.y+p.h,p.w,p.h)
     end
